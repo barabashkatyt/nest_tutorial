247670545f5c510e12ba6c91951c0dc3609854f3
balbatushka <balbatushka@gmail.com>
2020-12-28 17:01:49 +0300

    added schemas for products, added @nestjs/mongoose, made connection to Mongodb

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,44 +1,12 @@
-import { Controller, Get, Post, Param, Delete, Put, Body, Redirect, HttpCode, HttpStatus, Header } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { CreateProductDto } from './products/dto/create-product.dto';
-import { UpdateProductDto } from './products/dto/update-product.dto';
 
-
-
-
-@Controller('products')
+@Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
 
   @Get()
-  // Reditect with status 301
-  // @Redirect('https://google.com', 301)
-  getAll() :string {
-    return "getaAll"
-  }
-
-  // @Get(':id')
-  // getOne(@Param() params){
-  //   return "one" + params.id
-  // }
-
-    @Get(':id')
-  getOne(@Param('id') id: string) :string{
-    return "one" + id
-  }
-
-  @Post()
-  @HttpCode(HttpStatus.CREATED)
-  @Header('Cache-control','none')
-  create(@Body() createProductDto: CreateProductDto) : string{
-    return `Title: ${createProductDto.title} Price: ${createProductDto.price}`
-  }
-  @Delete(':id')
-  remove(@Param('id') id :string){
-    return 'Remove' + id
-  }
-
-  @Put(':id')
-  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string){
-    return 'Update' + id
+  getHello(): string {
+    return 'Hello NestJS!'
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './products/products.controller';
+import { ProductsModule } from './products/products.module';
+import { ProductsService } from './products/products.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProductsController],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(`mongodb+srv://bara:tyt123@cluster0.0o3do.mongodb.net/products?retryWrites=true&w=majority`)
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
